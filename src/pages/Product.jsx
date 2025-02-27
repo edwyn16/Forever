368eacf379452e1eea6bd971fb318a04f61ed286
Edwyn Jones <edwyn__4@hotmail.com>
2025-02-27 20:10:48 +0100

    created state variable where the products are stored.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/shopContext'
 
 const Product = () => {
   const {productId} = useParams()
-  
+  const {products} = useContext(ShopContext)
 
   return (
     <div>
