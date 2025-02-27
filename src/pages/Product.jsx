583d363184d37b0ccf987554830037dc802ab6d0
Edwyn Jones <edwyn__4@hotmail.com>
2025-02-27 21:18:55 +0100

    UI being created for displaying the products.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
+import { assets } from '../assets/frontend_assets/assets'
 
 const Product = () => {
   const {productId} = useParams()
@@ -14,6 +15,7 @@ const Product = () => {
       if (item._id === productId) {
         setProductData(item)
         setImage(item.image[0])
+        console.log(item);
         
         return null
       }
@@ -25,8 +27,27 @@ const Product = () => {
   }, [productId, products])
 
   return productData ? (
-    <div>
-        
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        {/**  Product Images */}
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' src={item} key={index} alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img className='w-full h-auto' src={image} alt="" />
+          </div>
+        </div>
+        {/**  Product Info */}
+        <div className='flex-1'>
+            <h1 className='font-medium text-2xl mt-2'> {productData.name} </h1>
+        </div>
+      </div>
     </div>
   ) : <div className='opacity-0'></div>
 }
