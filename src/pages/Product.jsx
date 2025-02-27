bed40cc258befeb6ff196b9484b07af694f1c4c6
Edwyn Jones <edwyn__4@hotmail.com>
2025-02-27 20:51:47 +0100

    fetchProductData function is created and tested via console.log to see if product-data is fetched and displayed in the console.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
 
@@ -8,7 +8,14 @@ const Product = () => {
   const [productData, setProductData] = useState(false)
 
   const fetchProductData = async () => {
-    
+    products.map((item) => {
+      if (item._id === productId) {
+        setProductData(item)
+        console.log(item);
+        
+        return null
+      }
+    })
   }
 
   useEffect(() => {
