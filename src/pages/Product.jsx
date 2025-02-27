d9bc6ef8267a6f9dba35c2c481cad79f096666af
Edwyn Jones <edwyn__4@hotmail.com>
2025-02-27 20:12:24 +0100

    created state variable where the product-data is stored which is initialized as false.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
 
 const Product = () => {
   const {productId} = useParams()
   const {products} = useContext(ShopContext)
+  const [productData, setProductData] = useState(false)
 
   return (
     <div>
