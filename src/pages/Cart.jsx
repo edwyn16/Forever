5e6815b70cc060ff7d9bfb28e9729f175fc5ff43
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-02 18:34:48 +0100

    useEffect created with loops to check if items quantity is greater than zero, if so join all the data into a new array.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,31 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/shopContext'
+import Title from '../components/Title'
 
 const Cart = () => {
   const {products, currency, cartItems } = useContext(ShopContext)
 
   const [cartData, setCartData] = useState([])
 
+  useEffect(() => {
+    const temporaryData = []
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          temporaryData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item]
+          })
+        }        
+      }
+    }
+    setCartData(temporaryData);
+    
+  }, [cartItems])
+
   return (
     <div>
-        
     </div>
   )
 }
