73c67ac7bcec39f46841ff9e6bd821210f06919f
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-02 18:26:34 +0100

    cartData useState is created and initialized with an empty array.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ShopContext } from '../context/shopContext'
 
 const Cart = () => {
   const {products, currency, cartItems } = useContext(ShopContext)
+
+  const [cartData, setCartData] = useState([])
 
   return (
     <div>
