dd49f5cd1fcaf4791ed03e2178146cc62399c576
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-02 18:22:48 +0100

    getCartCount function is created.

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -32,6 +32,21 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getCartCount = () => {
+        let totalCount = 0
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item]
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        } return totalCount
+    }
+
     useEffect(() => {
         console.log(cartItems);
     }, [cartItems])
@@ -39,7 +54,7 @@ const ShopContextProvider = (props) => {
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
-        cartItems, addToCart
+        cartItems, addToCart, getCartCount
     }
 
     return (
