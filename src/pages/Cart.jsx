1710b87393775d1946b6559b93b96a1b85a4c062
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-04 19:03:42 +0100

    cartTotal UI is now mounted in the Cart file.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/shopContext'
 import Title from '../components/Title'
 import { assets } from '../assets/frontend_assets/assets'
+import CartTotal from '../components/CartTotal'
 
 const Cart = () => {
   const {products, currency, cartItems, updateQuantity } = useContext(ShopContext)
@@ -52,6 +53,11 @@ const Cart = () => {
             )
           })
         }
+      </div>
+      <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+        </div>
       </div>
     </div>
   )
