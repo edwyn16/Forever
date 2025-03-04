6fe3494f0934dee1e494e295d5b39d85470d129b
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-04 18:56:25 +0100

    UI created for the cart total amount will be displayed.

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -1,12 +1,31 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/shopContext'
+import Title from './Title'
 
 const CartTotal = () => {
     const {currency, delivery_fee, getCarAmount} = useContext(ShopContext)
 
   return (
-    <div>
-        
+    <div className='w-full'>
+        <div className="text-2xl">
+            <Title text1={'CART'} text2={'TOTALS'} />
+        </div>
+        <div className='flex flex-col gap-2 mt-2 text-sm'>
+            <div className='flex justify-between'>
+                <p> Subtotal </p>
+                <p> { currency } { getCarAmount() }.00 </p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <p> Shipping Fee </p>
+                <p> { currency } { delivery_fee } </p>
+            </div>
+            <hr />
+            <div className='flex justify-between'>
+                <b> Total </b>
+                <b> { currency } { getCarAmount() === 0 ? 0 : getCarAmount() + delivery_fee } </b>
+            </div>
+        </div>
     </div>
   )
 }
