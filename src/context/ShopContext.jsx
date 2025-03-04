3bf42651404c5d297c9256055f62baaf5462136b
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-04 18:48:22 +0100

    async function passed into the value object.

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -72,7 +72,7 @@ const ShopContextProvider = (props) => {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart, getCartCount,
-        updateQuantity
+        updateQuantity, getCartAmount
     }
 
     return (
