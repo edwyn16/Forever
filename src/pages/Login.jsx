d8c92ee91916a7a0600ee0cb9652268a7852f47c
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-06 19:47:54 +0100

    base UI is created.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,15 @@ const Login = () => {
   const [currentState, setCurrentState] = useState('Sign Up')
 
   return (
-    <div>
-        
-    </div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+        <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+          <p className='prata-regular text-3xl'> {currentState} </p>
+          <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+        </div>
+        <input className='w-full px-3 py-2 border border-gray-800' type="text" placeholder='Name' required />
+        <input className='w-full px-3 py-2 border border-gray-800' type="email" placeholder='Email' required />
+        <input className='w-full px-3 py-2 border border-gray-800' type="password" placeholder='Password' required />
+    </form>
   )
 }
 
