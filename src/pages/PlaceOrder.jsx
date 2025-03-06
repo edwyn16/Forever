6b24ff337063caadd9d868974d5e2b772ba5dd1d
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-06 18:48:30 +0100

    place order button created.

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -40,17 +40,20 @@ const PlaceOrder = () => {
           {/** ----- Payment Method Selection ----- */}
           <div className='flex gap-3 flex-col lg:flex-row'>
             <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === '' ? 'bg-green-400' : ''}`}>  </p>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}>  </p>
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
             </div>
-            <div onClick={() => setMethod('')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === '' ? 'bg-green-400' : ''}`}>  </p>
+            <div onClick={() => setMethod('-')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === '-' ? 'bg-green-400' : ''}`}>  </p>
               <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
             </div>
             <div onClick={() => setMethod('COD')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === '' ? 'bg-green-400' : ''}`}>  </p>
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'COD' ? 'bg-green-400' : ''}`}>  </p>
               <p className='text-gray-500 text-sm font-medium mx-4'> CASH ON DELIVERY </p>
             </div>
+          </div>
+          <div className='w-full text-end mt-8'>
+            <button className='bg-black text-white px-16 py-3 text-sm'> PLACE ORDER </button>
           </div>
         </div>
       </div>
