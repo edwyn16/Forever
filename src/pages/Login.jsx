402dbfd42e9160ebf996473a5e109cff53f2a8fe
Edwyn Jones <edwyn__4@hotmail.com>
2025-03-06 19:36:58 +0100

    useState created and initialized with Sign Up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState('Sign Up')
+
   return (
     <div>
         
